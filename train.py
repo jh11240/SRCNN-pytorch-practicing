be1f26337eefa63dee5e39481daf8cce3067038c
JH Seo <73728655+jh11240@users.noreply.github.com>
2023-11-23 11:35:51 +0900

    train/utils upload

--- train.py
+++ train.py
@@ -11,16 +11,17 @@ from tqdm import tqdm
 
 from models import SRCNN
 from datasets import TrainDataset, EvalDataset
-from utils import AverageMeter, calc_psnr
+from utils import AverageMeter, calc_psnr, SSIMLoss, grayscale_ssim
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--train-file', type=str, required=True)
-    parser.add_argument('--eval-file', type=str, required=True)
-    parser.add_argument('--outputs-dir', type=str, required=True)
+    parser.add_argument('--train-file', type=str, default="HanokModel/hanokModel.h5")
+    parser.add_argument('--eval-file', type=str, default="HanokModel/hanokEval.h5")
+    parser.add_argument('--outputs-dir', type=str, default="HanokModel/model")
     parser.add_argument('--scale', type=int, default=3)
     parser.add_argument('--lr', type=float, default=1e-4)
+    parser.add_argument('--loss', type=str, default="SSIM")
     parser.add_argument('--batch-size', type=int, default=16)
     parser.add_argument('--num-epochs', type=int, default=400)
     parser.add_argument('--num-workers', type=int, default=8)
@@ -40,6 +41,7 @@ if __name__ == '__main__':
     model = SRCNN().to(device)
     model.load_state_dict(torch.load("srcnn_x2.pth", map_location=device))
     criterion = nn.MSELoss()
+
     optimizer = optim.Adam([
         {'params': model.conv1.parameters()},
         {'params': model.conv2.parameters()},
@@ -56,9 +58,11 @@ if __name__ == '__main__':
     eval_dataset = EvalDataset(args.eval_file)
     eval_dataloader = DataLoader(dataset=eval_dataset, batch_size=1)
 
-    best_weights = copy.deepcopy(model.state_dict())
+    best_weights_psnr = copy.deepcopy(model.state_dict())
+    best_weights_ssim = copy.deepcopy(model.state_dict())
     best_epoch = 0
     best_psnr = 0.0
+    best_ssim = 0
 
     for epoch in range(args.num_epochs):
         model.train()
@@ -75,7 +79,10 @@ if __name__ == '__main__':
 
                 preds = model(inputs)
 
-                loss = criterion(preds, labels)
+                if args.loss == "MSE":
+                    loss = criterion(preds, labels)
+                elif args.loss == "SSIM":
+                    loss = SSIMLoss(preds, labels, device)
 
                 epoch_losses.update(loss.item(), len(inputs))
 
@@ -90,6 +97,7 @@ if __name__ == '__main__':
 
         model.eval()
         epoch_psnr = AverageMeter()
+        epoch_ssim = AverageMeter()
 
         for data in eval_dataloader:
             inputs, labels = data
@@ -101,13 +109,23 @@ if __name__ == '__main__':
                 preds = model(inputs).clamp(0.0, 1.0)
 
             epoch_psnr.update(calc_psnr(preds, labels), len(inputs))
+            epoch_ssim.update(grayscale_ssim(preds, labels), len(inputs))
 
         print('eval psnr: {:.2f}'.format(epoch_psnr.avg))
+        print('eval ssim: {:.2f}'.format(epoch_ssim.avg))
 
         if epoch_psnr.avg > best_psnr:
             best_epoch = epoch
             best_psnr = epoch_psnr.avg
-            best_weights = copy.deepcopy(model.state_dict())
+            best_weights_psnr = copy.deepcopy(model.state_dict())
+
+        if epoch_ssim.avg > best_ssim:
+            best_epoch = epoch
+            best_ssim = epoch_ssim.avg
+            best_weights_ssim = copy.deepcopy(model.state_dict())
 
     print('best epoch: {}, psnr: {:.2f}'.format(best_epoch, best_psnr))
-    torch.save(best_weights, os.path.join(args.outputs_dir, 'best.pth'))
+    torch.save(best_weights_psnr, os.path.join(args.outputs_dir, 'best_psnr.pth'))
+
+    print('best epoch: {}, ssim: {:.2f}'.format(best_epoch, best_psnr))
+    torch.save(best_weights_ssim, os.path.join(args.outputs_dir, 'best_ssim.pth'))
--- utils.py
+++ utils.py
@@ -1,7 +1,6 @@
 import torch
 import numpy as np
 
-
 def convert_rgb_to_y(img):
     if type(img) == np.ndarray:
         return 16. + (64.738 * img[:, :, 0] + 129.057 * img[:, :, 1] + 25.064 * img[:, :, 2]) / 256.
@@ -50,6 +49,46 @@ def convert_ycbcr_to_rgb(img):
 def calc_psnr(img1, img2):
     return 10. * torch.log10(1. / torch.mean((img1 - img2) ** 2))
 
+def rgb_ssim(img1, img2):
+    ssim = 0
+
+    for i in range(3):
+        avg_x = np.mean(img1[:, :, i])
+        avg_y = np.mean(img2[:, :, i])
+
+        std_x = np.std(img1[:, :, i])
+        std_y = np.std(img2[:, :, i])
+
+        cov_xy = np.cov(img1[:, :, i].flatten(), img2[:, :, i].flatten())[0, 1]
+
+        ssim += (2 * avg_x * avg_y + 6.5025) * (2 * cov_xy + 58.5225) / (avg_x ** 2 + avg_y ** 2 + 6.5025) / (std_x ** 2 + std_y ** 2 + 58.5225)
+
+    return ssim / 3
+
+
+def grayscale_ssim(img1, img2):
+    img1 = img1.numpy()
+    img2 = img2.detach().numpy()
+
+    avg_x = np.mean(img1)
+    avg_y = np.mean(img2)
+
+    std_x = np.std(img1)
+    std_y = np.std(img2)
+
+    cov_xy = np.cov(img1.flatten(), img2.flatten())[0, 1]
+
+    ssim = (2 * avg_x * avg_y + 0.0255) * (2 * cov_xy + 0.2295) / (avg_x ** 2 + avg_y ** 2 + 0.0255) / (std_x ** 2 + std_y ** 2 + 0.2295)
+
+    return ssim
+
+
+def SSIMLoss(Y, Y_pred, device):
+    loss = 1 - grayscale_ssim(Y_pred, Y)
+
+    loss_tensor = torch.tensor(loss, requires_grad=True).to(device)
+
+    return loss_tensor
 
 class AverageMeter(object):
     def __init__(self):
@@ -66,3 +105,4 @@ class AverageMeter(object):
         self.sum += val * n
         self.count += n
         self.avg = self.sum / self.count
+
